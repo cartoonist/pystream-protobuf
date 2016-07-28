0ee6553844b80cb259ee2f93921f0f4d461bfe79
Ali Ghaffaari <ali.ghaffaari@mpi-inf.mpg.de>
2016-07-29 01:49:37 +0200

    Published in PyPi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pystream-protobuf',
+    packages=['stream'],
+    version='0.9',
+    description='Python implementation of stream library',
+    author='Ali Ghaffaari',
+    author_email='ali.ghaffaari@gmail.com',
+    url='https://github.com/cartoonist/pystream-protobuf',
+    download_url='https://github.com/cartoonist/pystream-protobuf/tarball/0.9',
+    keywords=['protobuf', 'stream', 'protocol buffer'],
+    classifiers=[],
+)
