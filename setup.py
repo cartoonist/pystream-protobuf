4f1f03fa0e14971c59e4fed15ae6ab2b65342cd4
Ali Ghaffaari <ali.ghaffaari@mpi-inf.mpg.de>
2016-07-29 13:30:39 +0200

    Use setuptools instead of distutils
    - Add package dependencies
    - Fix Manifest file

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='pystream-protobuf',
     packages=['stream'],
-    version='0.9',
+    version='0.9.1',
     description='Python implementation of stream library',
     author='Ali Ghaffaari',
     author_email='ali.ghaffaari@gmail.com',
     url='https://github.com/cartoonist/pystream-protobuf',
-    download_url='https://github.com/cartoonist/pystream-protobuf/tarball/0.9',
+    download_url='https://github.com/cartoonist/pystream-protobuf/tarball/0.9.1',
     keywords=['protobuf', 'stream', 'protocol buffer'],
     classifiers=[],
+    install_requires=['protobuf==3.0.0b4'],
 )
